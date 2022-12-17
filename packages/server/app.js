41e90c664c3b9a56c6fd7fddd33ab0a1e20d2a09
Justus Fluegel <justusfluegel@gmail.com>
2022-12-18 00:48:05 +0100

    fix(server): Allow for BrowserRouter routing
    Signed-off-by: Justus Fluegel <justusfluegel@gmail.com>

--- packages/server/app.js
+++ packages/server/app.js
@@ -1,20 +1,28 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
-const { PORT, CLIENT_APPLICATION, checkEnvVars } = require('./env_vars');
-require('dotenv').config({ path: './.env' });
+const { PORT, CLIENT_APPLICATION, checkEnvVars } = require("./env_vars");
+require("dotenv").config({ path: "./.env" });
 checkEnvVars();
 
-const api = require('./api');
+const api = require("./api");
 
 const app = express();
 
 app.use(cors());
+// Serve static files from the React app
+app.use(express.static(CLIENT_APPLICATION));
 app.use(express.json());
 
-app.use('/api', api);
-app.use('', express.static(CLIENT_APPLICATION));
+// Put all API endpoints under '/api'
+app.use("/api", api);
+
+// Handle all requests that don't match the ones above, for example those created by a browser router
+app.get("*", (_, res) => {
+    res.sendFile(path.join(CLIENT_APPLICATION, "index.html"));
+});
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+    console.log(`listening on port ${PORT}`);
 });
